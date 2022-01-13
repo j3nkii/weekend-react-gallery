66efe9b3946fab4688bcf23f6b8db0d59acc77c5
Jacob Larson <jacob.larson55112@gmail.com>
2022-01-13 17:54:31 -0600

    added axios GET

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from'axios';
+import GalleryList from '../GalleryList/GalleryList'
 import './App.css';
 
+
+
 function App() {
+
+  const [galleryList, setGalleryList] = useState([]);
+
+  axios.get('/gallery').then(res => {
+    console.log('GETTING /gallery', res.data);
+    setGalleryList(res.data)
+  }).catch(err => {
+    console.log('ERR GETTING /gallery', err);
+  })
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <GalleryList />
       </div>
     );
 }
---
+++ src/components/GalleryList/GalleryList.jsx
@@ -0,0 +1,10 @@
+function GalleryList(){
+    return (
+        <>
+            <p>Gallery goes here</p>
+            <img src="images/goat_small.jpg"/>
+        </>
+    )
+}
+
+export default GalleryList;
