f2e2b4fde8d7e40561743f486d95ce77d451012b
Jacob Larson <jacob.larson55112@gmail.com>
2022-01-13 20:19:30 -0600

    Added a swap image feature, styling is sub par, but is functinoal

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,9 +1,19 @@
+import {useState} from 'react'
+
 function GalleryItem({item}){
     console.log('in item', item);
+
+    const [isClick, setIsClick] = useState(false)
+
+    const onImageClick = () =>{
+        console.log('this is image', );
+        setIsClick(!isClick)
+    }
+
     return (
         <>
             <div className="gallery-item">
-                    <img src={item.path} />
+                    {isClick ? <p onClick={onImageClick}>{item.description}</p> : <img onClick={onImageClick} src={item.path} />}
                     <button>love it!</button>
                     {item.likes > 0 
                         ? <p>{item.likes} people like this!</p> 
