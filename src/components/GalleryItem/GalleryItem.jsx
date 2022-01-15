d71b21f27fbeb223b4caaa72bcca5af18cd3071a
Jacob Larson <jacob.larson55112@gmail.com>
2022-01-15 10:01:48 -0600

    Added mui masonry

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,6 +1,23 @@
 import {useState} from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Paper from '@mui/material/Paper';
+import Masonry from '@mui/lab/Masonry';
+import { styled } from '@mui/material/styles';
 
-function GalleryItem({item, addLikes, removeIMG}){
+const Label = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    color: theme.palette.text.secondary,
+    border: '1px solid black',
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+function GalleryItem({item, addLikes, removeIMG, index}){
     console.log('in item', item);
 
     const [isClick, setIsClick] = useState(false)
@@ -17,19 +34,24 @@ function GalleryItem({item, addLikes, removeIMG}){
         removeIMG(item.id);
     }
 
+
     return (
-        <>
-            <div className="gallery-item">
-                    {isClick 
-                        ? <p onClick={onImageClick}>{item.description}</p> 
-                        : <img onClick={onImageClick} src={item.path} height="200px" width="auto"/>}
+        <Stack key={index}>
+            <Label>BUTT</Label>
+            {isClick 
+            ? <p onClick={onImageClick}>{item.description}</p>
+            : <img
+                src={`${item.path}?w=250&auto=format`}
+                srcSet={`${item.path}?w=250&auto=format&dpr=2 2x`}
+                loading="lazy"
+                style={{ borderBottomLeftRadius: 4, borderBottomRightRadius: 4 }}
+            />}
                     <button onClick={onAddLikes}>love it!</button>
                     <button onClick={onRemoveIMG}>Delete</button>
                     {item.likes > 0 
                         ? <p>{item.likes} people like this!</p> 
                         : <p>{item.likes} people like this :(</p>}
-                </div>
-        </>
+        </Stack>
     )
 }
 
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,17 +1,47 @@
 import GalleryItem from '../GalleryItem/GalleryItem'
+//muistuff i hope
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Paper from '@mui/material/Paper';
+import Masonry from '@mui/lab/Masonry';
+import { styled } from '@mui/material/styles';
+
+const Label = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    color: theme.palette.text.secondary,
+    border: '1px solid black',
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+
+
+
+
+
+
 
 function GalleryList({addLikes, galleryList, removeIMG}){
     console.log(galleryList);
     return (
         <div>
-            {galleryList.map((item) => ( 
-                <GalleryItem 
-                    key={item.id} 
-                    item={item} 
-                    addLikes={addLikes} 
-                    removeIMG={removeIMG}
-                />
-            ))}
+            <Box sx={{ width: 500, minHeight: 829 }}>
+                <Masonry columns={3} spacing={1}>
+                    {galleryList.map((item, index) => ( 
+                        <GalleryItem 
+                            key={item.id} 
+                            item={item} 
+                            addLikes={addLikes} 
+                            removeIMG={removeIMG}
+                            index={index}
+                        />
+                    ))}
+                </Masonry>
+            </Box>
         </div>
     )
 }
